90303a7f35742a2ff8a33ec99564da8fc4f18c0f
kaihere14 <yashpalsharma9950@gmail.com>
2026-04-24 00:20:28 +0530

    ci: add type checking to pre-commit hook and fix type annotation in globe component

--- client/components/ui/globe.tsx
+++ client/components/ui/globe.tsx
@@ -160,7 +160,7 @@ export function Globe({ globeConfig, data }: WorldProps) {
       .arcStartLng((d) => (d as { startLng: number }).startLng * 1)
       .arcEndLat((d) => (d as { endLat: number }).endLat * 1)
       .arcEndLng((d) => (d as { endLng: number }).endLng * 1)
-      .arcColor((e) => (e as Position).color)
+      .arcColor((e: unknown) => (e as Position).color)
       .arcAltitude((e) => (e as { arcAlt: number }).arcAlt * 1)
       .arcStroke(() => [0.32, 0.28, 0.3][Math.round(Math.random() * 2)])
       .arcDashLength(defaultProps.arcLength)
