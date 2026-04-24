8812c83bf4cc57247c708c69598b676b1170c405
kaihere14 <yashpalsharma9950@gmail.com>
2026-04-24 12:20:06 +0530

    refactor: remove GitHub authentication SQL migration script

--- server/drizzle/0007_github_auth.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "client_table" ALTER COLUMN "google_id" DROP NOT NULL;
---> statement-breakpoint
-ALTER TABLE "client_table" ADD COLUMN "github_id" varchar(255) UNIQUE;
---> statement-breakpoint
-ALTER TABLE "user_table" ALTER COLUMN "google_id" DROP NOT NULL;
---> statement-breakpoint
-ALTER TABLE "user_table" ADD COLUMN "github_id" varchar(255) UNIQUE;
